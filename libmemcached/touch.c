3c23b8e6f6cd8bb2d51127f0e2b83125c9f58795
Sergey Avseyev <sergey.avseyev@gmail.com>
2011-06-15 11:29:00 +0300

    fetch service instance using master_key

--- libmemcached/touch.c
+++ libmemcached/touch.c
@@ -5,18 +5,36 @@ memcached_return_t memcached_touch(memcached_st *ptr,
                                    const char *key, size_t key_length,
                                    time_t expiration)
 {
+  return memcached_touch_by_key(ptr, key, key_length, key, key_length, expiration);
+}
+
+memcached_return_t memcached_touch_by_key(memcached_st *ptr,
+                                          const char *master_key, size_t master_key_length,
+                                          const char *key, size_t key_length,
+                                          time_t expiration)
+{
   if ((ptr->flags.binary_protocol) == 0)
     return MEMCACHED_NOT_SUPPORTED;
 
   memcached_return_t rc;
+  uint32_t master_server_key;
 
   LIBMEMCACHED_MEMCACHED_TOUCH_START();
   unlikely (memcached_server_count(ptr) == 0)
     return MEMCACHED_NO_SERVERS;
 
-  rc= memcached_validate_key_length(key_length, ptr->flags.binary_protocol);
-  unlikely (rc != MEMCACHED_SUCCESS)
-    return rc;
+  unlikely (ptr->flags.verify_key && (memcached_key_test(&key, &key_length, 1) == MEMCACHED_BAD_KEY_PROVIDED))
+    return MEMCACHED_BAD_KEY_PROVIDED;
+
+  if (!(master_key && master_key_length))
+  {
+    master_key = key;
+    master_key_length = key_length;
+  }
+  unlikely (ptr->flags.verify_key && (memcached_key_test((const char * const *)&master_key, &master_key_length, 1) == MEMCACHED_BAD_KEY_PROVIDED))
+    return MEMCACHED_BAD_KEY_PROVIDED;
+
+  master_server_key= memcached_generate_hash_with_redistribution(ptr, master_key, master_key_length);
 
   protocol_binary_request_touch request= {.bytes= {0}};
   request.message.header.request.magic= PROTOCOL_BINARY_REQ;
@@ -27,21 +45,18 @@ memcached_return_t memcached_touch(memcached_st *ptr,
   request.message.header.request.bodylen= htonl((uint32_t)(key_length + ptr->prefix_key_length + request.message.header.request.extlen));
   request.message.body.expiration = htonl((uint32_t) expiration);
 
-  for (uint32_t x= 0; x < memcached_server_count(ptr); x++)
+  memcached_server_write_instance_st instance= memcached_server_instance_fetch(ptr, master_server_key);
+  struct libmemcached_io_vector_st vector[]=
   {
-    memcached_server_write_instance_st instance= memcached_server_instance_fetch(ptr, x);
-    struct libmemcached_io_vector_st vector[]=
-    {
-      { .length= sizeof(request.bytes), .buffer= request.bytes },
-      { .length= ptr->prefix_key_length, .buffer= ptr->prefix_key },
-      { .length= key_length, .buffer= key }
-    };
+    { .length= sizeof(request.bytes), .buffer= request.bytes },
+    { .length= ptr->prefix_key_length, .buffer= ptr->prefix_key },
+    { .length= key_length, .buffer= key }
+  };
 
-    if ((rc= memcached_vdo(instance, vector, 3, true)) != MEMCACHED_SUCCESS)
-    {
-      memcached_io_reset(instance);
-      return (rc == MEMCACHED_SUCCESS) ? MEMCACHED_WRITE_FAILURE : rc;
-    }
+  if ((rc= memcached_vdo(instance, vector, 3, true)) != MEMCACHED_SUCCESS)
+  {
+    memcached_io_reset(instance);
+    return (rc == MEMCACHED_SUCCESS) ? MEMCACHED_WRITE_FAILURE : rc;
   }
 
   LIBMEMCACHED_MEMCACHED_TOUCH_END();
